3a398beb288f68aeca5444eb3c055d9b2af06303
Adam Ernst <adamjernst@gmail.com>
2020-12-10 20:42:08 -0600

    Strip down and start to build up new approach

--- monitor.py
+++ monitor.py
@@ -1,30 +1,24 @@
-from apscheduler.schedulers.background import BackgroundScheduler
 import argparse
-import datetime
-import importlib
 import gatt
 import logging
-import os
-import traceback
-import yaml
 
-from controllers.base_controller import BaseController
+
+BUTTON_STATUS_SERVICE_UUID = '99c31523-dc4f-41b1-bb04-4e4deb81fadd'
 
 
 logger = logging.getLogger('monitor')
 
-manager = gatt.DeviceManager(adapter_name='hci0')
 
-print_log = False
+class TurnTouchDeviceManager(gatt.DeviceManager):
+    def device_discovered(self, device):
+        super().device_discovered(device)
+        logger.info("Discovered [%s] %s" % (device.mac_address, device.alias()))
 
-def log(msg, level=logging.INFO):
-    if print_log:
-        print(msg)
-    else:
-        logger.log(level, msg)
+    def make_device(self, mac_address):
+        return TurnTouchDevice(mac_address=mac_address, manager=self)
 
 
-class TurnTouch(gatt.Device):
+class TurnTouchDevice(gatt.Device):
 
     button_codes = {
         b'\xff\x00': 'Off',
@@ -42,35 +36,22 @@ class TurnTouch(gatt.Device):
         b'\xf7\xff': 'South Hold'
     }
 
-    button_presses = []
-
+    battery_status_characteristic = None
     battery_notifications_sent = []
-
-    def __init__(self, mac_address, manager, buttons, name, controllers, default_action, press_only):
-        super().__init__(mac_address, manager)
-        self.sched = BackgroundScheduler()
-        self.sched.start()
-        self.button_actions = buttons
-        self.listening = False
-        self.name = name
-        self.controllers = controllers
-        self.default_action = default_action
-        self.press_only = press_only
-        self.battery_status_characteristic = None
 
     def connect_succeeded(self):
         super().connect_succeeded()
-        log("Connected!")
+        logger.info("Connected: %s", self.mac_address)
 
     def connect_failed(self, error):
         super().connect_failed(error)
-        log("Connect failed with error {}".format(error))
+        logger.info("Connecting to %s failed with error %s", self.mac_address, error)
 
     def services_resolved(self):
         super().services_resolved()
         button_status_service = next(
             s for s in self.services
-            if s.uuid == '99c31523-dc4f-41b1-bb04-4e4deb81fadd'
+            if s.uuid == BUTTON_STATUS_SERVICE_UUID
         )
 
         self.button_status_characteristic = next(
@@ -104,7 +85,7 @@ class TurnTouch(gatt.Device):
 
     def characteristic_enable_notifications_succeeded(self, characteristic):
         super().characteristic_enable_notifications_succeeded(characteristic)
-        log("Connected to {}!".format(self.name))
+        logger.info("Characteristic notifications enabled for %s", self.mac_address)
 
     def characteristic_value_updated(self, characteristic, value):
         super().characteristic_value_updated(characteristic, value)
@@ -114,133 +95,30 @@ class TurnTouch(gatt.Device):
             if self.button_actions.get(key, False) and key not in self.battery_notifications_sent:
                 self.battery_notifications_sent.append(key)
                 self.perform('battery', str(percentage))
-            log('Battery status: {}%'.format(percentage))
+            logger.info('Battery status for %s: %s%%', self.mac_address, percentage)
             return
         if value == b'\xff\x00': #off
             return
-        if self.press_only:
-            direction, action = self.button_codes[value].split(' ')
-            if action == 'Press':
-                self.perform(direction, action)
-            return
-        self.button_presses.append(value)
-        if not self.listening:
-            self.listening = True
-            time = datetime.datetime.now() + datetime.timedelta(seconds=1)
-            self.sched.add_job(self.deduplicate_buttons, trigger='date', run_date=time)
-
-    def deduplicate_buttons(self):
-        self.listening = False
-        actions = [self.button_codes[p] for p in self.button_presses]
-        # work out what to do
-        first_words = [s.split(' ')[0] for s in actions]
-        second_words = [s.split(' ')[1] for s in actions]
-        self.button_presses = []
-        if len(set(first_words)) != 1:
-            log("Too many presses too quickly")
-            return
-        direction = first_words[0]
-        if 'Double' in second_words:
-            self.perform(direction, 'Double')
-        elif 'Hold' in second_words:
-            self.perform(direction, 'Hold')
-        else:
-            self.perform(direction, 'Press')
-
-    def perform(self, direction, action):
-        log("Performing {} {}".format(direction, action))
-        action = self.button_actions.get("{}_{}".format(direction.lower(), action.lower()), {'type': 'none'})
-        log("Action: {}".format(action))
-
-        try:
-            if action['type'] in self.controllers:
-                self.controllers[action['type']].perform(action)
-            elif self.default_action:
-                self.controllers[self.default_action].perform(action)
-            elif action['type'] == 'none':
-                return
-            else:
-                log("No controller found for action {}".format(action['type']))
-        except Exception as ex:
-            log("{}\ncaught exception from controller action. we'll ignore it and continue.".format(traceback.format_exc()))
-
-
-def get_controllers():
-    res = []
-    for f in os.listdir("./controllers"):
-        if 'controller' in f and f != 'base_controller.py':
-            m = importlib.import_module('controllers.{}'.format(os.path.splitext(f)[0]))
-            controller = [c for _, c in m.__dict__.items() if (type(c) == type) and c != BaseController and issubclass(c, BaseController)][0]
-            res.append((f.split('_')[0], controller))
-    return res
-
+        direction, action = self.button_codes[value].split(' ')
+        if action == 'Press':
+            self.perform(direction, action)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--print", help="print output, rather than logging",
-                    action="store_true")
-    parser.add_argument("-l", "--list", help="list available controllers",
-                    action="store_true")
-    parser.add_argument("-c", "--controller", type=str, help="show help for a controller")
-    parser.add_argument("-s", "--setup", type=str, help="run setup for a controller")
     args = parser.parse_args()
 
-    if args.print:
-        print_log = True
-    else:
-        logging.basicConfig(filename='/var/log/turntouch.log',
-            filemode='a',
-            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-            datefmt='%H:%M:%S',
-            level=logging.INFO)
+    logging.basicConfig(
+        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+        datefmt='%H:%M:%S',
+        level=logging.INFO)
 
-    if args.list:
-        print("Controllers:")
-        for controller in get_controllers():
-            print(" - [{}]: {}".format(controller[0], controller[1].get_class_name()))
-    elif args.controller:
-        controllers = get_controllers()
-        try:
-            controller = [m for m in controllers if m[0] == args.controller][0]
-            print(controller[1].help())
-        except:
-            print("A controller called {} was not found. The available controllers are:".format(args.controller))
-            for controller in controllers:
-                print(" - {}".format(controller[0]))
-    elif args.setup:
-        controllers = get_controllers()
-        try:
-            controller = [m for m in controllers if m[0] == args.setup][0]
-        except:
-            print("A controller called {} was not found. The available controllers are:".format(args.controller))
-            for controller in controllers:
-                print(" - {}".format(controller[0]))
-        controller[1](print=True).print_all()
-    else:
-        try:
-            with open('config.yml') as f:
-                config = yaml.safe_load(f)
-                log('Config loaded: {}'.format(config))
-        except Exception as e:
-            config = []
-            log("Error loading config: {}".format(e))
-        for c in config:
-            controllers = {}
-            for t in set([b['type'] for _, b in c['buttons'].items()]):
-                log("Found command of type {}, trying to load controller".format(t))
-                m = importlib.import_module('controllers.{}_controller'.format(t))
-                controller = [c for _, c in m.__dict__.items() if (type(c) == type) and c != BaseController and issubclass(c, BaseController)][0]
-                controllers[t] = controller(print=print_log)
-            device = TurnTouch(
-                    mac_address=c['mac'],
-                    manager=manager,
-                    buttons=c['buttons'],
-                    name=c['name'],
-                    controllers=controllers,
-                    default_action=c.get('default_action'),
-                    press_only=c.get('press_only', False)
-            )
-            log("Trying to connect to {} at {}...".format(c['name'], c['mac']))
-            device.connect()
-        manager.run()
+    manager = TurnTouchDeviceManager(adapter_name='hci0')
+    manager.start_discovery([BUTTON_STATUS_SERVICE_UUID])
 
+    # device = TurnTouch(
+    #     mac_address=c['mac'],
+    #     manager=manager,
+    # )
+    # logger.info("Trying to connect to {} at {}...".format(c['name'], c['mac']))
+    # device.connect()
+    manager.run()
